63214e75e3bb1d452ff5f06eefd925da56546ef5
Junho Lee <junholee369@gmail.com>
2022-06-23 15:04:27 -0500

    New Capstone

---
+++ Inventory_Management_Capstone/src/main/java/com/example/Inventory_Management_Capstone/InventoryManagementCapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.Inventory_Management_Capstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryManagementCapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryManagementCapstoneApplication.class, args);
+	}
+
+}
---
+++ Inventory_Management_Capstone/src/test/java/com/example/Inventory_Management_Capstone/InventoryManagementCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Inventory_Management_Capstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagementCapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
